115988a18042356004dbd86768ac8f03c6be818b
wkabia12 <wkabia12@gmail.com>
2023-08-01 11:04:36 +0000

    feat: added 9: getheigh t and 10: get depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures depth of binary tree.
+ * @tree: A pointer to the root node of the tree to traverse.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (tree == NULL)
+		return 0;
+	else if (tree->parent == NULL)
+		return 0;
+	else
+	{
+		depth = binary_tree_depth(tree->parent);
+	}
+	
+	return 1 + depth;
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures height of binary tree.
+ * @tree: A pointer to the root node of the tree to traverse.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height, r_height;
+
+	if (tree == NULL)
+		return 0;
+	else if (tree->left == NULL && tree->right == NULL)
+		return 0;
+	else
+	{
+		l_height = binary_tree_height(tree->left);
+		r_height = binary_tree_height(tree->right);
+	}
+	
+	if (l_height >= r_height)
+		return 1 + l_height;
+	else
+		return 1 + r_height;
+}
